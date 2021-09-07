314f70ddd25e97b36cd63450fd70acae9a39e614
ANNAmiuz <yingyihuang2000@outlook.co>
2021-09-07 18:16:30 +0800

    good version passing the test

--- proxy.c
+++ proxy.c
@@ -5,7 +5,7 @@
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
-#define MAX_CACHE MAX_CACHE_SIZE / MAX_OBJECT_SIZE
+#define MAX_CACHE 10 //why not MAX_CACHE_SIZE/MAX_OBJECT_SIZE?!!!
 #define DEBUGx
 
 /* You won't lose style points for including this long line in your code */
